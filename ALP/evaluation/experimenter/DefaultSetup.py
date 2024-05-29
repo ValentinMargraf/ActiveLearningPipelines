30ab0f9ec6942fd290b3b2814b992c1b04b2a8a0
Valentin Margraf <valentin.margraf@lmu.de>
2024-05-29 17:53:28 +0200

    small and dynamic setting

--- ALP/evaluation/experimenter/DefaultSetup.py
+++ ALP/evaluation/experimenter/DefaultSetup.py
@@ -1,26 +1,33 @@
-from sklearn.ensemble import (
-    ExtraTreesClassifier,
-    GradientBoostingClassifier,
-    RandomForestClassifier,
-)
+import catboost as cb
+import xgboost as xgb
+from pytorch_tabnet.tab_model import TabNetClassifier
+from sklearn.ensemble import ExtraTreesClassifier, GradientBoostingClassifier, RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
+from tabpfn import TabPFNClassifier
 
 from ALP.benchmark.BenchmarkConnector import MySQLBenchmarkConnector
 from ALP.pipeline.SamplingStrategy import (
-    BatchBaldSampling,
+    BALDSampling,
+    ClusterMargin,
+    CoreSetSampling,
     DiscriminativeSampling,
     EntropySampling,
-    ExpectedAveragePrecision,
+    EpistemicUncertaintySamplingStrategy,
+    FalcunSampling,
+    KMeansSampling,
     LeastConfidentSampling,
     MarginSampling,
+    MaxEntropySampling,
     MinMarginSampling,
     MonteCarloEERLogLoss,
     MonteCarloEERMisclassification,
+    PowerBALDSampling,
     PowerMarginSampling,
+    QBCVarianceRatioSampling,
     QueryByCommitteeEntropySampling,
     QueryByCommitteeKLSampling,
     RandomMarginSampling,
@@ -32,80 +39,110 @@ from ALP.pipeline.SamplingStrategy import (
 
 def ensure_default_setup(dbbc: MySQLBenchmarkConnector):
     # init settings
-    settings = [{
-        'name': 'small',
-        'labeled_train_size': 30,
-        'train_type': 'absolute',
-        'test_size': 0.33,
-        'number_of_samples': 1,
-        'number_of_iterations': 20
-    }, {
-        'name': 'medium',
-        'labeled_train_size': 100,
-        'train_type': 'absolute',
-        'test_size': 0.33,
-        'number_of_samples': 5,
-        'number_of_iterations': 20
-    }, {
-        'name': 'large-10',
-        'labeled_train_size': 300,
-        'train_type': 'absolute',
-        'test_size': 0.33,
-        'number_of_samples': 5,
-        'number_of_iterations': 20
-    }, {
-        'name': 'large-20',
-        'labeled_train_size': 100,
-        'train_type': 'absolute',
-        'test_size': 0.33,
-        'number_of_samples': 5,
-        'number_of_iterations': 20
-    }]
+    settings = [
+        {
+            "name": "small",
+            "labeled_train_size": 30,
+            "train_type": "absolute",
+            "test_size": 0.33,
+            "number_of_samples": 10,
+            "number_of_iterations": 20,
+            "factor": None,
+        },
+        {
+            "name": "medium",
+            "labeled_train_size": 100,
+            "train_type": "absolute",
+            "test_size": 0.33,
+            "number_of_samples": 50,
+            "number_of_iterations": 20,
+            "factor": None,
+        },
+        {
+            "name": "large",
+            "labeled_train_size": 300,
+            "train_type": "absolute",
+            "test_size": 0.33,
+            "number_of_samples": 200,
+            "number_of_iterations": 20,
+            "factor": None,
+        },
+        {
+            "name": "small_dynamic",
+            "labeled_train_size": 10,
+            "train_type": "absolute",
+            "test_size": 0.33,
+            "number_of_samples": 10,
+            "number_of_iterations": 20,
+            "factor": 5,
+        },
+        {
+            "name": "large_dynamic",
+            "labeled_train_size": 10,
+            "train_type": "absolute",
+            "test_size": 0.33,
+            "number_of_samples": 10,
+            "number_of_iterations": 20,
+            "factor": 20,
+        },
+    ]
     for setting in settings:
         dbbc.load_or_create_setting(**setting)
 
     default_sampling_strategies = {
         "random": RandomSamplingStrategy(42),
         "random_margin": RandomMarginSampling(42),
+        "cluster_margin": ClusterMargin(42),
+        "core_set": CoreSetSampling(42),
+        "epistemic": EpistemicUncertaintySamplingStrategy(42),
         "entropy": EntropySampling(42),
+        "falcun": FalcunSampling(42),
         "margin": MarginSampling(42),
+        "max_entropy": MaxEntropySampling(42, 10),
         "least_confident": LeastConfidentSampling(42),
         "mc_logloss": MonteCarloEERLogLoss(42),
         "mc_misclass": MonteCarloEERMisclassification(42),
+        "kmeans": KMeansSampling(42),
         "discrim": DiscriminativeSampling(42),
         "qbc_entropy": QueryByCommitteeEntropySampling(42, 10),
         "qbc_kl": QueryByCommitteeKLSampling(42, 10),
-        "bald": BatchBaldSampling(42, 10),
+        "bald": BALDSampling(42, 10),
         "power_margin": PowerMarginSampling(42),
-        "min_margin": MinMarginSampling(42),
-        "expected_avg": ExpectedAveragePrecision(42),
+        "min_margin": MinMarginSampling(42, 10),
         "typ_cluster": TypicalClusterSampling(42),
-        "weighted_cluster": WeightedClusterSampling(42)
+        "weighted_cluster": WeightedClusterSampling(42),
+        "qbc_variance_ratio": QBCVarianceRatioSampling(42, 10),
+        "power_bald": PowerBALDSampling(42, 10),
     }
+
     for k, obj in default_sampling_strategies.items():
         dbbc.load_or_create_sampling_strategy(k, obj)
 
     default_learners = {
-        "svm_lin": SVC(kernel='linear', probability=True),
-        "svm_rbf": SVC(kernel='rbf', probability=True),
-        "rf_entropy": RandomForestClassifier(n_estimators=100, max_depth=10, criterion='entropy'),
-        "rf_gini": RandomForestClassifier(n_estimators=100, max_depth=10, criterion='gini'),
-        "rf_entropy_large": RandomForestClassifier(n_estimators=250, max_depth=10, criterion='entropy'),
-        "rf_gini_large": RandomForestClassifier(n_estimators=250, max_depth=10, criterion='gini'),
+        "svm_lin": SVC(kernel="linear", probability=True),
+        "svm_rbf": SVC(kernel="rbf", probability=True),
+        "rf_entropy": RandomForestClassifier(n_estimators=100, max_depth=10, criterion="entropy"),
+        "rf_gini": RandomForestClassifier(n_estimators=100, max_depth=10, criterion="gini"),
+        "rf_entropy_large": RandomForestClassifier(n_estimators=250, max_depth=10, criterion="entropy"),
+        "rf_gini_large": RandomForestClassifier(n_estimators=250, max_depth=10, criterion="gini"),
         "knn_3": KNeighborsClassifier(n_neighbors=3),
         "knn_10": KNeighborsClassifier(n_neighbors=10),
         "log_reg": LogisticRegression(),
         "multinomial_bayes": MultinomialNB(),
-        "etc_entropy": ExtraTreesClassifier(n_estimators=100, max_depth=10, criterion='entropy'),
-        "etc_gini": ExtraTreesClassifier(n_estimators=100, max_depth=10, criterion='gini'),
-        "etc_entropy_large": ExtraTreesClassifier(n_estimators=250, max_depth=10, criterion='entropy'),
-        "etc_gini_large": ExtraTreesClassifier(n_estimators=250, max_depth=10, criterion='gini'),
+        "etc_entropy": ExtraTreesClassifier(n_estimators=100, max_depth=10, criterion="entropy"),
+        "etc_gini": ExtraTreesClassifier(n_estimators=100, max_depth=10, criterion="gini"),
+        "etc_entropy_large": ExtraTreesClassifier(n_estimators=250, max_depth=10, criterion="entropy"),
+        "etc_gini_large": ExtraTreesClassifier(n_estimators=250, max_depth=10, criterion="gini"),
         "naive_bayes": GaussianNB(),
         "mlp": MLPClassifier(),
         "GBT_logloss": GradientBoostingClassifier(n_estimators=100),
-        "GBT_exp": GradientBoostingClassifier(n_estimators=100, loss='exponential'),
+        "GBT_exp": GradientBoostingClassifier(n_estimators=100, loss="exponential"),
         "GBT_logloss_large": GradientBoostingClassifier(n_estimators=250),
-        "GBT_exp_large": GradientBoostingClassifier(n_estimators=250, loss='exponential')
+        "tabpfn": TabPFNClassifier(device="cpu", N_ensemble_configurations=32),
+        "xgb": xgb.XGBClassifier(tree_method="hist", max_depth=6, n_estimators=100),
+        "catboost": cb.CatBoostClassifier(iterations=500, depth=6, verbose=0, rsm=0.1),
+        "tabnet": TabNetClassifier(verbose=0),
     }
+
     for k, obj in default_learners.items():
         dbbc.load_or_create_learner(k, obj)
--- ALP/evaluation/experimenter/LogTableObserver.py
+++ ALP/evaluation/experimenter/LogTableObserver.py
@@ -10,12 +10,36 @@ class LogTableObserver(StatisticalPerformanceObserver):
         self.result_processor = result_processor
 
     def observe_data(self, iteration, X_u_selected, y_u_selected, X_l_aug, y_l_aug, X_u_red):
-        eval_scores = super().compute_labeling_statistics(iteration, X_u_selected, y_u_selected, X_l_aug, y_l_aug,
-                                                          X_u_red)
-        self.result_processor.process_logs(
-            {LogTableObserver.labeling_log_tbl: eval_scores})
+        eval_scores = super().compute_labeling_statistics(
+            iteration, X_u_selected, y_u_selected, X_l_aug, y_l_aug, X_u_red
+        )
+        self.result_processor.process_logs({LogTableObserver.labeling_log_tbl: eval_scores})
 
     def observe_model(self, iteration, model):
         eval_scores = super().compute_model_performances(iteration, model)
-        self.result_processor.process_logs(
-            {LogTableObserver.model_performance_tbl: eval_scores})
+        self.result_processor.process_logs({LogTableObserver.model_performance_tbl: eval_scores})
+
+
+class SparseLogTableObserver(StatisticalPerformanceObserver):
+    model_performance_tbl = "accuracy_log"
+    labeling_log_tbl = "labeling_log"
+
+    def __init__(self, result_processor, X_test, y_test):
+        super().__init__(X_test, y_test)
+        self.result_processor = result_processor
+
+    def observe_data(self, iteration, X_u_selected, y_u_selected, X_l_aug, y_l_aug, X_u_red):
+        eval_scores = super().compute_labeling_statistics(
+            iteration, X_u_selected, y_u_selected, X_l_aug, y_l_aug, X_u_red
+        )
+        return eval_scores
+
+    def observe_model(self, iteration, model):
+        eval_scores = super().compute_model_performances(iteration, model)
+        return eval_scores
+
+    def log_data(self, dict):
+        self.result_processor.process_logs({LogTableObserver.labeling_log_tbl: dict})
+
+    def log_model(self, dict):
+        self.result_processor.process_logs({LogTableObserver.model_performance_tbl: dict})
