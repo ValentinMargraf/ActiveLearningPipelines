7f69d1fb1548f828ce2f8df6dff6033886e99962
Marcel Wever <marcel.wever@ifi.lmu.de>
2024-06-13 10:18:07 +0200

    Fixed local import for fixtures in the tests package.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,3 @@
-from fixtures.scenario import benchmark_connector, scenario, setting
+from .fixtures.scenario import benchmark_connector, scenario, setting
 
 __all__ = ["setting", "scenario", "benchmark_connector"]
